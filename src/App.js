6708fbe464f5dc5ea13c666dbbdef1a5f3b2179e
vishnuramv <vishnu.v1902@gmail.com>
2021-04-18 03:10:45 +0530

    Add: Previous Prediction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import SymptomsForm from "./pages/SymptomsForm";
+import SymptomResult from "./pages/SymptomResult";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Router>
+				<Switch>
+					<Route exact path="/login" component={Login} />
+					<Route path="/sign-up" component={SignUp} />
+					{/* Private routes => only available if logged in */}
+					<PrivateRoute exact path="/" component={Home} />
+					<PrivateRoute exact path="/symptom-form" component={SymptomsForm} />
+					<PrivateRoute exact path="/symptom-result" component={SymptomResult} />
+				</Switch>
+			</Router>
+		</>
+	);
 }
 
 export default App;
